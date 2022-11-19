bc2ad2ef7bb58c70288fb19a8887f762e2755c66
olechka27 <olechka.dobrova@gmail.com>
2022-11-19 17:51:38 +0500

    Initial commit

---
+++ src/main/java/employee_requests/http/emloyee_book_spring_boot/EmloyeeBookSpringApplication.java
@@ -0,0 +1,14 @@
+package employee_requests.http.emloyee_book_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmloyeeBookSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EmloyeeBookSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/employee_requests/http/emloyee_book_spring_boot/EmloyeeBookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package employee_requests.http.emloyee_book_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmloyeeBookSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
